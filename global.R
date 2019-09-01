e6360272d7817fc459b7c41793b340fd83bc2316
Felix Benning <felix.benning@gmail.com>
2019-09-01 17:31:47 +0200

    fixed length warning, colored the graphs

--- global.R
+++ global.R
@@ -3,6 +3,30 @@ library(jsonlite)
 
 budgetEstimation <- read_json("budgetEstimations.json", simplifyVector = TRUE)
 
+calcSwitchParams <-function(budgetYears, switchTime) {
+  logBase <- (budgetYears-switchTime-sqrt((budgetYears-switchTime)^2+4*(switchTime*budgetYears-switchTime^2/2)))/(2*switchTime*budgetYears-switchTime^2)
+  linFactor <- logBase/(1-logBase*switchTime)
+  expFactor <- exp(-logBase*switchTime)/(1-logBase*switchTime)
+  
+  params <- data.frame(linFactor = linFactor, logBase = logBase, expFactor = expFactor)
+  return(params)
+}
+
+calcLinParams <- function(budgetYears) {
+  factor<- -1/(2*budgetYears)
+  zeroPoint <- 2*budgetYears
+  params <- data.frame(linFactor = factor, zeroPoint = zeroPoint)
+  return(params)
+}
+
+switchFunc <- function(x, switchTime, params) {
+  y <- (x<switchTime)*(params$linFactor*x+1) + (x>=switchTime)*params$expFactor*exp(params$logBase*x)
+}
+linearFunc <- function(x, params) {
+  return((params$linFactor*x+1)*(x<params$zeroPoint))
+}
+
+
 
 currentYearDecimal <- function() {
   return(as.double(format(Sys.Date(), "%Y")) + as.double(format(Sys.Date(), "%m"))/12 + as.double(format(Sys.Date(), "%d"))/365)
--- server.R
+++ server.R
@@ -1,4 +1,3 @@
-library(shiny)
 library(ggplot2)
 
 #server.R
@@ -24,36 +23,42 @@ function(input,output, session){
     }
   })
   
-  ydelta <- reactive({input$switchYear-currentYearDecimal()})
-  logBase <- reactive({
-    (euBudgY()-ydelta()-sqrt((euBudgY()-ydelta())^2+4*(ydelta()*euBudgY()-ydelta()^2/2)))/(2*ydelta()*euBudgY()-ydelta()^2)
-  })
-  linFactor <- reactive({logBase()/(1-logBase()*ydelta())})
-  expFactor <- reactive({exp(-logBase()*ydelta())/(1-logBase()*ydelta())})
+  ydelta <- reactive({
+    if(is.null(input$switchYear)) {
+      2030 -currentYearDecimal()
+    } else {
+      input$switchYear-currentYearDecimal()
+    }
+    
+    })
+  
+  switchParams <- reactive({calcSwitchParams(euBudgY(), ydelta())})
+  linParams <- reactive({calcLinParams(euBudgY())})
+  
   
   output$euBudgY <-renderText({
     sprintf("Budget of EU: %.1f years of current EU CO2 production", euBudgY())
   })
   
   output$plot<-renderPlot({
-    t<-seq(0,50,length=100)
-    y<-(t<ydelta())*(linFactor()*t+1) + (t>=ydelta())*expFactor()*exp(logBase()*t)
-    factor<--1/(2*euBudgY())
-    linear<-factor*t+1
     
-    df<-data.frame(year=c(t,t)+currentYearDecimal(),emissions=c(y,linear)*100)
-    ggplot(df, aes(x=year, y=emissions))+
-      geom_line(linetype = "dashed")+
-      geom_point()+
-      ylim(0,100)
+    t<-seq(0,50,length=500)
+    linear <- linearFunc(t, linParams())
+    switch <- switchFunc(t, ydelta(), switchParams())
+    
+    df<-data.frame(year=t+currentYearDecimal(), linear=linear*100, switch = switch*100)
+    ggplot(df)+
+      geom_line(aes(x=year, y=linear, colour = "linear"))+
+      geom_line(aes(x=year, y=switch, colour = "switch"))+
+      ylim(0,100)+guides(colour = guide_legend("Type"))
   })
   output$reductionFactors<-renderText({
     sprintf("%.2f %% (percentage points) reduction in the first %.2f years until %.0f. 
             Then exponential decrease by %.2f%%", 
-            -linFactor()*100,
+            -switchParams()$linFactor*100,
             ydelta(), 
             ydelta()+currentYearDecimal(), 
-            (1-exp(logBase()))*100
+            (1-exp(switchParams()$logBase))*100
     )
   })
   output$switchLinExp <- renderUI({
